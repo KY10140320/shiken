767cfec13ada701aaf0570abf4a4367a1d6599d7
KY10140320 <ploject.itoh1014@icloud.com>
2023-03-07 02:34:22 +0900

    auaaa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+const val_hum = 2.3; //人文科学
+
+
+
+
+
+
 //クロック
 const time_limit = () => {
   const pad = (num) => (num < 10 ? '0' : '') + num; //表記を二桁に固定
@@ -23,3 +30,10 @@ const updateClock = () => {
   setTimeout(updateClock, 1000); //1000はマジックナンバー
   };
   updateClock();
+
+
+//進捗
+const pad2 = (num) => (Math.floor(num) < 10 ? '0' : '') + num.toFixed(2);
+const max_hum = document.getElementById("prog_hum").max;
+document.getElementById("prog_hum").value = val_hum;
+document.getElementById("per_hum").innerText = `${pad2(val_hum * 100 / max_hum)}%`;
