5b165ef4765309faadf19716dcdb3aacb324c48c
KY10140320 <ploject.itoh1014@icloud.com>
2023-03-08 00:32:33 +0900

    qawsedrftgyh

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
+const val_math = 196; //数的処理
 const val_hum = 2.3; //人文科学
 
 
 
 
-
+const ragnarok = "2023/04/09"
 
 //クロック
 const time_limit = () => {
   const pad = (num) => (num < 10 ? '0' : '') + num; //表記を二桁に固定
-  const deadline = new Date("2023/04/15");
+  const deadline = new Date(ragnarok);
   const now = new Date();
   let diff = (deadline - now);
 
@@ -34,6 +35,12 @@ const updateClock = () => {
 
 //進捗
 const pad2 = (num) => (Math.floor(num) < 10 ? '0' : '') + num.toFixed(2);
+
+const max_math = document.getElementById("prog_math").max;
+document.getElementById("prog_math").value = val_math;
+document.getElementById("per_math").innerText = `${pad2(val_math * 100 / max_math)}%`;
+
 const max_hum = document.getElementById("prog_hum").max;
 document.getElementById("prog_hum").value = val_hum;
 document.getElementById("per_hum").innerText = `${pad2(val_hum * 100 / max_hum)}%`;
+
