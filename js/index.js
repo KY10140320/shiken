aa6db574c45a63aa141eedc0d7de8dbba5d1fb55
KY10140320 <ploject.itoh1014@icloud.com>
2023-03-15 02:30:25 +0900

    進捗割合の実装

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const val_math = 260; //数的処理
-const val_socio = 3.0; //人文科学
-
+const val_socio = 3; //人文科学
+const val_cur = 0;
 
 
 
@@ -35,12 +35,28 @@ const updateClock = () => {
 
 //進捗
 const pad2 = (num) => (Math.floor(num) < 10 ? '0' : '') + num.toFixed(2);
+const to3Count = (str) => {
+  var strPad0 = str.replaceAll("\u00a0", "0");
+  if (strPad0.length >= 3) {
+    console.log(str)
+    return str
+  }
+  //\u00A0
+  return to3Count("\u00a0"+str);
+};
 
 const max_math = document.getElementById("prog_math").max;
 document.getElementById("prog_math").value = val_math;
+document.getElementById("ratio_math").innerText = `${to3Count(val_math.toString())}/${to3Count(max_math.toString())}`;
 document.getElementById("per_math").innerText = `${pad2(val_math * 100 / max_math)}%`;
 
 const max_socio = document.getElementById("prog_socio").max;
 document.getElementById("prog_socio").value = val_socio;
+document.getElementById("ratio_socio").innerText = `${to3Count(val_socio.toString())}/${to3Count(max_socio.toString())}`;
 document.getElementById("per_socio").innerText = `${pad2(val_socio * 100 / max_socio)}%`;
+
+const max_cur = document.getElementById("prog_cur").max;
+document.getElementById("prog_cur").value = val_cur;
+document.getElementById("ratio_cur").innerText = `${to3Count(val_cur.toString())}/${to3Count(max_cur.toString())}`;
+document.getElementById("per_cur").innerText = `${pad2(val_cur * 100 / max_cur)}%`;
 
