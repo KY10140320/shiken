24111bcf8d89f8131a5fd1f5715c3775197e1862
KY10140320 <ploject.itoh1014@icloud.com>
2023-04-18 10:38:01 +0900

    にじ

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
-const val_math = 260; //数的処理
-const val_socio = 6; //人文科学
-const val_cur = 0;
-const val_deg = 0;
 
-
-
-const ragnarok = "2023/04/09"
+const ragnarok = "2023/05/07"
 
 //クロック
 const time_limit = () => {
@@ -34,35 +28,5 @@ const updateClock = () => {
   updateClock();
 
 
-//進捗
-const pad2 = (num) => (Math.floor(num) < 10 ? '0' : '') + num.toFixed(2);
-const to3Count = (str) => {
-  var strPad0 = str.replaceAll("\u00a0", "0");
-  if (strPad0.length >= 3) {
-    console.log(str)
-    return str
-  }
-  //\u00A0
-  return to3Count("\u00a0"+str);
-};
 
-const max_math = document.getElementById("prog_math").max;
-document.getElementById("prog_math").value = val_math;
-document.getElementById("ratio_math").innerText = `${to3Count(val_math.toString())}/${to3Count(max_math.toString())}`;
-document.getElementById("per_math").innerText = `${pad2(val_math * 100 / max_math)}%`;
-
-const max_socio = document.getElementById("prog_socio").max;
-document.getElementById("prog_socio").value = val_socio;
-document.getElementById("ratio_socio").innerText = `${to3Count(val_socio.toString())}/${to3Count(max_socio.toString())}`;
-document.getElementById("per_socio").innerText = `${pad2(val_socio * 100 / max_socio)}%`;
-
-const max_cur = document.getElementById("prog_cur").max;
-document.getElementById("prog_cur").value = val_cur;
-document.getElementById("ratio_cur").innerText = `${to3Count(val_cur.toString())}/${to3Count(max_cur.toString())}`;
-document.getElementById("per_cur").innerText = `${pad2(val_cur * 100 / max_cur)}%`;
-
-const max_deg = document.getElementById("prog_deg").max;
-document.getElementById("prog_deg").value = val_deg;
-document.getElementById("ratio_deg").innerText = `${to3Count(val_deg.toString())}/${to3Count(max_deg.toString())}`;
-document.getElementById("per_deg").innerText = `${pad2(val_deg * 100 / max_deg)}%`;
 
